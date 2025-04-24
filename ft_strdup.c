43bcbe17b7770f9538315a47336ca6add4d0ec13
Uliana Nadorozhna <unadoroz@c1r5p7.hive.fi>
2025-04-24 14:06:25 +0300

    added strdup

---
+++ ft_strdup.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unadoroz <unadoroz@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 13:52:38 by unadoroz          #+#    #+#             */
+/*   Updated: 2025/04/24 14:05:31 by unadoroz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+char	*ft_strdup(const char *s)
+{
+	char	*new;
+	int		i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	new = (char *)malloc(sizeof(char) * (i + 1));
+	if (new == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+#include <stdio.h>
+int     main(void)
+{
+        printf("%s\n", ft_strdup("Hello, friends!"));
+		printf("%s\n", strdup("Hello, friends!"));
+
+}
