a794614ca6dda126d182f6942bc19281c075f28a
Uliana Nadorozhna <unadoroz@c1r5p7.hive.fi>
2025-04-25 12:41:16 +0300

    updated makefile and libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: unadoroz <unadoroz@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 12:05:15 by unadoroz          #+#    #+#             */
-/*   Updated: 2025/04/23 12:13:21 by unadoroz         ###   ########.fr       */
+/*   Updated: 2025/04/25 10:56:14 by unadoroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,14 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t ft_strlen (const char *str);
 size_t	ft_strlcpy (char *dst, const char *src, size_t size);
 size_t	strlcat(char *dst, const char *src, size_t size);
-
+char *ft_strchr(const char *s, int c);
+char *ft_strrchr(const char *s, int c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+char *ft_strnstr(const char *big, const char *little, size_t n);
+int	ft_atoi(const char *nptr);
+char	*ft_strdup(const char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
